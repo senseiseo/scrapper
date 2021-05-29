26a54b5aa5428e031d5e1e6ce14c09ba9f688734
senseiseo <1919mur@gmail.com>
2021-05-29 21:49:55 +0300

    Тестовая версия скреппера

--- scrapper/scrapper.rb
+++ scrapper/scrapper.rb
@@ -6,7 +6,7 @@ def scraper
     url = "https://funpay.ru/"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page.body)
-    game_listing = parsed_page.css('div.game-title-item') # 219 name of game 
+    game_listing = parsed_page.css('div.promo-game-item') # 219 name of game 
     game_listing.each  do |game_listing|
         game = {
             title: game_listing.css('div.game-title').text ,
