ced251a603d27b76ddf9f8e973ca225d560238eb
senseiseo <1919mur@gmail.com>
2021-05-29 21:30:51 +0300

    добавлены изменения

--- scrapper/scrapper.rb
+++ scrapper/scrapper.rb
@@ -5,9 +5,19 @@ require 'byebug'
 def scraper 
     url = "https://funpay.ru/"
     unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    byebug
+    parsed_page = Nokogiri::HTML(unparsed_page.body)
+    game_listing = parsed_page.css('div.game-title-item') # 219 name of game 
+    game_listing.each  do |game_listing|
+        game = {
+            title: game_listing.css('div.game-title').text ,
+            link:  game_listing.css('a')[1].text,
+            url:   game_listing.css('a')[0].attributes["href"].value
+        }
+            byebug
+    end 
 end
+# gameCards количесвто титлов div.game-title, gamePromo промо всего div.game-title-item
+
 
 
 scraper
